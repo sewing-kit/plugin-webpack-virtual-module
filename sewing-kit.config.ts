ed1e1f2d236371e7db7545094a49f6456be1f913
Chris Sauve <chrismsauve@gmail.com>
2019-12-24 04:06:43 -0500

    Upgrade sewing-kit

--- sewing-kit.config.ts
+++ sewing-kit.config.ts
@@ -1,17 +1,14 @@
 import {createPackage, Runtime} from '@sewing-kit/config';
-import {createPackageFlexibleOutputsPlugin} from '@sewing-kit/plugin-package-flexible-outputs';
-import {
-  quiltWorkspacePlugin,
-  quiltPackagePlugin,
-} from '@quilted/sewing-kit-plugins';
+import {buildFlexibleOutputs} from '@sewing-kit/plugin-package-flexible-outputs';
+import {quiltWorkspace, quiltPackage} from '@quilted/sewing-kit-plugins';
 
 export default createPackage((pkg) => {
   pkg.runtime(Runtime.Node);
   pkg.entry({root: './src/index'});
-  pkg.plugins(
-    quiltWorkspacePlugin,
-    quiltPackagePlugin,
-    createPackageFlexibleOutputsPlugin({
+  pkg.use(
+    quiltWorkspace(),
+    quiltPackage(),
+    buildFlexibleOutputs({
       binaries: false,
       commonjs: true,
       esmodules: false,
